2f948ad6ea19cef04db7302dbf68b43fa256655d
haomoy <haomoy@gmail.com>
2019-08-31 23:27:58 -0600

    Merge conflict and file leak fixed.

--- wordCount.py
+++ wordCount.py
@@ -8,75 +8,46 @@ import sys
 import re
 
 '''
+#Used for local testing.
+inputPath = 'C:/Users/haomo/Documents/CS/Classes/OS/lab1/python-intro-haomoy/declaration.txt'
+outputPath = 'C:/Users/haomo/Documents/CS/Classes/OS/lab1/python-intro-haomoy/output.txt'
+inputFile = open(inputFile,'r')
+outputFile = open(outputPath, 'w+')
+'''
+
 #Test case to check for the number of command line arguments
 if len(sys.argv)<2:
     print("You must feed the program an input and utput file, in that order.")
     exit()
     
-print("test passed")
-    
-#Input and output files are grabbed from the command line
-    #Input file is readable but not modifiable
-    #Output file is writable and if there is none, one will be created (this might be unnecessary due to the check in line 11.    
-print(sys.argv[1])
-print(sys.argv[2])
-
-inputFile = open(sys.argv[1],'r+') 
-outputFile = open(sys.argv[2],'w+') 
-
-print("files opened")
-'''
-
-inputPath = '/users/haomo/Documents/Classes/OS/lab1/python-intro-haomoy/declaration.txt'
-outputPath = '/users/haomo/Documents/Classes/OS/lab1/python-intro-haomoy/output.txt'
-inputFile = open(inputPath,'r')
-outputFile = open(outputPath, 'w+')
-
 #Declare dictionary to store word as key and its number of appareances as the value. 
 words = {}
 
-#Read input file as a single string
-#Proceed to delete whitespace and split by periods and spaces
-#Turn words into lower case 
-inputString = inputFile.read()
-print(inputString)
-inputString = inputString.strip()
-print(inputString)
-inputWords = re.split('[ \t]', inputString)
-print(inputWords)
-
-for word in inputWords: 
-    print("Printing words:")
-    print(word)
-    word = word.lower() 
-    print(word)
-    if (words[word]):
-        words[word] += 1
-    else: 
-        words[word] = 1 
-    print("End for loop")
-    
-'''
-for line in inputLines: 
-    print("Printing og lines:")
-    print(line)
-    line = re.sub(".", "\s", line)
-    print("Printing lines:")
-    print(line)
-    for word in re.split("\s", line):
-        word = word.lower()
-        print("Word:")
+#Input file is grabbed from the command line.
+#Input file is readable but not modifiable.
+#Encased in 'with as' to control the opening, processing, and closing of the file.
+with open(sys.argv[1],'r') as inputFile:
+    inputString = inputFile.read() #Read input file as a single string
+    inputString = inputString.rstrip() #Proceed to delete whitespace and split by periods and spaces
+    inputWords = re.split('[^a-zA-Z]', inputString) #Every character that is not alpha will mark the end of a word
+    for word in inputWords: 
+        word = word.lower() #Turn words into lower case 
         print(word)
-        words[word] += 1
-'''
-inputFile.close()
-print("input closed")
-#The dictionary listed and that lis is sorted in alphabetical order. 
-#Every key and value is printed into the output document.
-for word, occurences in sorted(words.items()): 
-    outputFile.write(word + " " + occurences)
+        if word: #Check for empty strings
+            if words.get(word): #Check if word has been seen before
+                words[word] += 1
+            else: 
+                words[word] = 1
 
-outputFile.close()
-
-print("end")
+#Output file is grabbed from the command line.
+#Input file is readable but not modifiable.
+#Encased in 'with as' to control the opening, processing, and closing of the file.
+with open(sys.argv[1],'r') as inputFile:
+    outputFile = open(sys.argv[2],'w+') 
+    #The dictionary is listed and that list is sorted in alphabetical order. 
+    #Every key and value is printed into the output document.
+    for word, occurences in sorted(words.items()): 
+        outputFile.write(word + " " + str(occurences) + "\n")
+    
+    
     
